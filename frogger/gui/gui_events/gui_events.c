73f369de9095a43b86dbad77c31283dcd15f7913
lucas <lucaskammann@hotmail.com>
2018-06-20 07:59:01 -0300

    event, event queue, create_queue

--- frogger/gui/gui_events/gui_events.c
+++ frogger/gui/gui_events/gui_events.c
@@ -1,6 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include "gui_events.h"
+#include <stdlib.h>
 
+/**********************/
+/* Objetos del modulo */
+/**********************/
+
+/********************************/
+/* Prototipo funciones privadas */
+/********************************/
+
+/************************************/
+/* Definicion de funciones privadas */
+/************************************/
+
+/************************************/
+/* Definicion de funciones publicas */
+/************************************/
+
+/* create_queue */
+EVENT_QUEUE* create_queue(void){
+    EVENT_QUEUE* queue;
+    
+    /* Reservo memoria para el handler */
+    queue = malloc( sizeof(EVENT_QUEUE) );
+    if( queue == NULL ){
+        return NULL;
+    }
+    
+    /* Reservo memoria para la cola de eventos */
+    queue->events = malloc( sizeof(EVENT) );
+    if( queue->events == NULL ){
+        return NULL;
+    }
+    
+    /* Inicializo parametros */
+    queue->nextEvent = queue->events;
+    queue->lastEvent = queue->events;
+    
+    /* Devuelvo cola creada */
+    return queue;
+}
--- frogger/gui/gui_events/gui_events.h
+++ frogger/gui/gui_events/gui_events.h
@@ -2,8 +2,40 @@
  * Este modulo permite manejar colas de eventos de forma dinamica.
  */
 
+/* Librerias utilizadas */
+#include <stdint.h>
+#include <stdbool.h>
+
 #ifndef GUI_EVENTS_H
 #define GUI_EVENTS_H
 
-#endif /* GUI_EVENTS_H */
+/******************/
+/* Tipos de datos */
+/******************/
 
+typedef struct{
+    uint32_t type;
+    uint32_t data;
+    uint32_t source;
+} EVENT;
+
+typedef struct{
+    /* Puntero a la cola */
+    EVENT* events;
+    
+    /* Puntero al proximo y ultimo elemento */
+    EVENT* nextEvent;
+    EVENT* lastEvent;
+} EVENT_QUEUE;
+
+/********************************/
+/* Prototipo funciones publicas */
+/********************************/
+
+/* create_queue 
+ * Reserva memoria inicial para trabajar con
+ * la cola de eventos y devuelve puntero 
+ */
+EVENT_QUEUE* create_queue(void);
+
+#endif /* GUI_EVENTS_H */
