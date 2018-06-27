761d9c3a883bfa0de77a44f4ed2d8278ee558471
lucas <lucaskammann@hotmail.com>
2018-06-26 21:36:39 -0300

    pausemenu rpi added

--- frogger/gui/frogger/rpi/rpi_output.c
+++ frogger/gui/frogger/rpi/rpi_output.c
@@ -57,6 +57,9 @@ bool frogger_mainmenu(void){
     /* Actualizo la pantalla */
     rpi_display_update();
 
+    /* Libero el bitmap */
+    destroy_bitmap(bitmap);
+
     /* Exitoso! */
     return true;
 
@@ -82,11 +85,69 @@ void frogger_mainmenu_move(INPUT_VALUES input){
 /* PAUSEMENU handlers */
 /**********************/
 
+/* Configuracion */
+#define PAUSEMENU_PATH   "gui/frogger/rpi/pausemenu/"
+#define PAUSEMENU_IMAGE  PAUSEMENU_PATH "pausemenu_"
+#define PAUSEBACK_IMAGE  PAUSEMENU_PATH "pauseback_"
+
+/* Variable del menu de pausa */
+PAUSEMENU_OPTIONS pausemenuSelection = RESUME_OPTION;
+
 /* frogger_pausemenu */
-bool frogger_pausemenu(void);
+bool frogger_pausemenu(void){
+    BITMAP* bitmap;
+    char text[MAX_STRING];
+
+    /* Limpio el display */
+    rpi_display_clear();
+
+    /* Armo el string para buscar bitmap de fondo */
+    sprintf(text, "%s%d.bmp", PAUSEBACK_IMAGE, pausemenuSelection);
+
+    /* Cargo el bitmap y lo imprimo */
+    bitmap = rpi_load_bitmap(text);
+    if( bitmap == NULL ){
+        return false;
+    }
+    rpi_draw_bitmap(bitmap, map_position(0, 0));
+
+    /* Libero el bitmap */
+    destroy_bitmap(bitmap);
+
+    /* Armo el string para buscar bitmap de opcion */
+    sprintf(text, "%s%d.bmp", PAUSEMENU_IMAGE, pausemenuSelection);
+
+    /* Cargo el bitmap y lo imprimo */
+    bitmap = rpi_load_bitmap(text);
+    if( bitmap == NULL ){
+        return false;
+    }
+    rpi_draw_bitmap(bitmap, map_position(0, 0));
+
+    /* Libero el bitmap */
+    destroy_bitmap(bitmap);
+
+    /* Actualizo el display */
+    rpi_display_update();
+
+    return true;
+}
 
 /* frogger_pausemenu_move */
-void frogger_pausemenu_move(INPUT_VALUES input);
+void frogger_pausemenu_move(INPUT_VALUES input){
+    switch( input ){
+        case MOVE_UP:
+            if( pausemenuSelection != RESUME_OPTION ){
+                pausemenuSelection -= 1;
+            }
+            break;
+        case MOVE_DOWN:
+            if( pausemenuSelection != PAUSE_EXIT_OPTION ){
+                pausemenuSelection += 1;
+            }
+            break;
+    }
+}
 
 /*************************/
 /* CHANGESCREEN handlers */
