fb62285adec902e5c673d8ebb641e7821d174f4b
lucas <lucaskammann@hotmail.com>
2018-06-27 17:42:26 -0300

    create_objects

--- frogger/gui/frogger/rpi/rpi_physics.c
+++ frogger/gui/frogger/rpi/rpi_physics.c
@@ -52,5 +52,41 @@ bool frogger_game_movement_valid(FROG frog, uint16_t input){
 
 /* frogger_game_create_object */
 ANIMATED_OBJECT* frogger_game_create_object(POSITION pos, SPEED speed, GUI_ANIMATION_ORIENTATION orientation, uint32_t type){
-    return NULL;
+    ANIMATED_OBJECT* object;
+
+    pos.x *= RPI_DISPLAY_STEP;
+    pos.y *= RPI_DISPLAY_STEP;
+
+    /* Cargo el object file */
+    switch( type ){
+        case FROGGER_MOTORBIKE:
+            object = gui_animation_load_object( RPI_MOTORBIKE_OBJFILE, pos, "" );
+            break;
+        case FROGGER_CAR:
+            object = gui_animation_load_object( RPI_CAR_OBJFILE, pos, "" );
+            break;
+        case FROGGER_TRUCK:
+            object = gui_animation_load_object( RPI_TRUCK_OBJFILE, pos, "" );
+            break;
+        case FROGGER_BOAT:
+            object = gui_animation_load_object( RPI_BOAT_OBJFILE, pos, "" );
+            break;
+        case FROGGER_YACHT:
+            object = gui_animation_load_object( RPI_YACHT_OBJFILE, pos, "" );
+            break;
+    }
+
+    /* Valido */
+    if( object == NULL ){
+        return NULL;
+    }
+
+    /* Cargo la velocidad */
+    object->speed = speed;
+
+    /* Cargo orientacion */
+    gui_animation_set_animation(object, gui_animation_seek_animation(object, orientation));
+
+    /* Devuelvo la creacion */
+    return object;
 }
